3b50258e9ef5b4d6bf8203da395dc61d33ad95b9
abraham23o <abraonyonka23@gmail.com>
2023-02-16 13:18:14 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-/*use the function puts
+/*
+ * use the function puts
  * not printf
- * should end with zero 
- * */
+ * should end with zero
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
