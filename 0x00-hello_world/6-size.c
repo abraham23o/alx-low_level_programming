82207f15bdbf12ea05aacee428cd5f61707fd95b
abraham23o <abraonyonka23@gmail.com>
2023-02-16 14:01:43 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,20 +4,17 @@
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
-    int a;
-    float b;
-    long int c;
-    char d;
-    long long int e;
+	int a;
+	float b;
+	long int c;
+	char d;
+	long long int e;
 
-    // sizeof evaluates the size of a variable
-    printf("Size of a char: %zu bytes\n", sizeof(d));
-    printf("Size of an int: %zu bytes\n", sizeof(a));
-    printf("Size of a long int: %zu bytes\n", sizeof(c));
-    printf("Size of a long long int: %zu bytes\n", sizeof(e));
-    printf("size of a float: %zu bytes\n", sizeof(b));
-    return 0;
+	printf("Size of an int: %lu bytes\n", sizeof(a)); 
+	printf("Size of a float: %lu bytes\n", sizeof(b));
+	printf("Size of a long int: %lu bytes\n", sizeof(c));
+	printf("Size of a char: %lu bytes\n", sizeof(d));
+	printf("Size of a long long int: %lu bytes\n", sizeof(e));
 }
-
