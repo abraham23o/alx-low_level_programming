e3e1036d504935b7aa247b048734739f1682de12
abraham23o <abraonyonka23@gmail.com>
2023-02-17 13:22:28 -0800

    5-print_numbers

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		if (x != 'e' && x != 'y')
+		{
+			putchar(x);
+		}
+		putchar('\n');
+		return (0);
+	}
+}
