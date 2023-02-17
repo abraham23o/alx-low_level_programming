9ea7070f235324efba34b3ba827601211817a02e
abraham23o <abraonyonka23@gmail.com>
2023-02-17 11:19:34 -0800

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int x = 'a'; x <= 'z'; x++)
+		putchar(x);
+		putchar('\n');
+}
