57f6c6b137291e80408e7093c0b325c8022e9935
abraham23o <abraonyonka23@gmail.com>
2023-02-23 13:56:53 -0800

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Print "FizzBuzz"
+ *
+ * Description: Print "Fizzbuzz" for numbers which are multiples
+ * of both 3 and 5, "Fizz" for multiples of 3
+ * and "Buzz" for multiples of 5
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 1; x < 100; x++)
+	{
+		if (x % 3 == 0)
+			printf("Fizz ");
+		else if (x % 5 == 0)
+			printf("Buzz ");
+		else if (x % 3 == 0 && x % 5 == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ", x);
+	}
+	printf("\n");
+
+	return (0);
+}
