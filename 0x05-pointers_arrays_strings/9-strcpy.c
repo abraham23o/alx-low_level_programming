044b2b00f1d7ccf52bc38ffe5dfa77d0e341feef
abraham23o <abraonyonka23@gmail.com>
2023-02-27 18:18:50 -0800

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcpy - copy src string to dest string
+ * @dest: string input
+ * @src: string input
+ *
+ * Return: character
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *temp = dest;
+
+	while (*src)
+		*dest = *src;
+
+	return (temp);
+}
