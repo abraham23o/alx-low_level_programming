c9dfc233baea7870d5b6553d93b4d0e631e2ea67
abraham23o <abraonyonka23@gmail.com>
2023-02-27 16:58:20 -0800

    prints half of aa string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * puts_half - Prints half of a string
+ * @str: string input
+ *
+ * Return: Nothing
+ */
+void puts_half(char *str)
+{
+	int len = 0, x, n;
+
+	while (str[len] != '\0')
+		len++;
+
+	if (len % 2 == 0)
+		n = len / 2;
+
+	else
+		n = (len - 1) / 2;
+
+	for (x = n; x < len; x++)
+		_putchar(str[x]);
+
+	_putchar('\n');
+}
+
