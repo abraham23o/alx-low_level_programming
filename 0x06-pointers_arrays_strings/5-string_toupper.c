f446a6a89b468459dd3746303902b17f9abd8ef4
abraham23o <abraonyonka23@gmail.com>
2023-03-03 13:21:39 -0800

    betty style resolve

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * string_toupper - Converts lowercase characters to uppercase
- * @x: string input
+ * @s: string input
  *
  * Return: capitalized string
  */
@@ -9,9 +9,9 @@ char *string_toupper(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++ )
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if( s[i] >= 'a' && s[i] <= 'z' )
+		if (s[i] >= 'a' && s[i] <= 'z')
 			s[i] = s[i] - 32;
 	}
 	return (s);
