f2a88b19739ef5e4b81aba5e5fcab04e9fa81761
abraham23o <abraonyonka23@gmail.com>
2023-03-05 04:42:48 -0800

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,9 +16,9 @@ int main(void)
 	{
 		if (x % 3 == 0 && x % 5 == 0)
 			printf("FizzBuzz ");
-		else if (x % 5 == 0)
+		else if (x % 5 == 0 && x % 3 != 0)
 			printf("Buzz ");
-		else if (x % 3 == 0)
+		else if (x % 3 == 0 && x % 5 != 0)
 			printf("Fizz ");
 		else
 			printf("%d ", x);
