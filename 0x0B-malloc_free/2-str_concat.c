6fab2e761936d9eb040b041d5fb6f5f66ca2a94c
abraham23o <abraonyonka23@gmai.com>
2023-06-14 03:45:46 -0700

    polish

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,7 +11,7 @@ char *str_concat(char *s1, char *s2)
 	char *word;
 	int i, j;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return ("");
 	i = j = 0;
 	while (s1[i] != '\0')
