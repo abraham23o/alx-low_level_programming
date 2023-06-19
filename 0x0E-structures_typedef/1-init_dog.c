aaf95739687832baa8e341b94802ce4015ddecad
abraham23o <abraonyonka23@gmai.com>
2023-06-19 11:56:12 -0700

    polish function to initialize dog struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 /**
  * init_dog - function that initializes a variable of type struct dog
  * @d: pointer variable of type struct dog
@@ -9,6 +10,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return;
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
