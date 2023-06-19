72e7154a7e2e388f99791d2e1ad720f0d26d3731
abraham23o <abraonyonka23@gmai.com>
2023-06-19 10:35:06 -0700

    create a header file and structure of type dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - dog structure
+ * @name: its name
+ * @age: its age
+ * @owner: its owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
