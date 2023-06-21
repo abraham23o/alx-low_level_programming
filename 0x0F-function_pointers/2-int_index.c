49042d8925229d359f18fbdaa00c51719c18fece
abraham23o <abraonyonka23@gmai.com>
2023-06-21 09:56:05 -0700

    polish function that searches for an int

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,7 +11,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	if (cmp == NULL || size <= 0)
+	if (array == NULL || cmp == NULL || size <= 0)
 		return (-1);
 	for (; i < size; i++)
 		if (cmp(array[i]))
