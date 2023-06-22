ba4be8cd037d442f8661f71ca3b4d504eae72ee8
abraham23o <abraonyonka23@gmai.com>
2023-06-22 06:56:36 -0700

    function pointers final task

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+/**
+ * op_add - adds two int
+ * @a: parameter to be added
+ * @b: parameter to be added
+ * Return: the sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two int
+ * @a: parameter to be subtracted
+ * @b: parameter to be subtracted
+ * Return: the difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two int
+ * @a: parameter to be multiplied
+ * @b: parameter to be multiplied
+ * Return: the product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides two int
+ * @a: parameter to be divided
+ * @b: parameter to be divided
+ * Return: the division of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod -gets the remainder of the division two int
+ * @a: int input
+ * @b: int input
+ * Return: the remainder of the division of a and b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
