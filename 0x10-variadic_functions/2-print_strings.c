b5f896b7958248b7458a1c24bc30a0d058346785
abraham23o <abraonyonka23@gmai.com>
2023-06-22 07:59:20 -0700

    polish function that prints strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (ptr_str == NULL)
 			printf("(nil)");
 		printf("%s", ptr_str);
-		if (i != n - 1 && separator != NULL)
+		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 	printf("\n");
