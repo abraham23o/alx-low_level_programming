542f40b3b1ee4777229ec17462e622ed6dec4b3a
abraham23o <abraonyonka23@gmail.com>
2023-10-11 00:48:22 +0300

    refactor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
-#define FILENAME __FILE__
-
 /**
  * main - Entry point
  *
  * Return: 0, success
 */
-int main (void)
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
