c234a6ee1365d0cad0d708a66e0e690a888590e0
abraham23o <abraonyonka23@gmail.com>
2023-10-11 20:44:50 +0300

    add readme file; print name function pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * print_name - function to receive a function as a parameter
+ * @name: name to be printed / changed to uppercase
+ * @*f: function pointer
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
