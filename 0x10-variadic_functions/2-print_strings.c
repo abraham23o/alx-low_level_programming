2520471fdeff8504d2a16a42ce5749a911da8f16
abraham23o <abraonyonka23@gmail.com>
2023-10-12 21:46:53 +0300

    refactor print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,9 +9,24 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
+	char *str;
 	va_list arr_ch;
 
 	va_start(arr_ch, n);
 
 	for (i = 0; i < n; i++)
+	{
+		str = va_arg(arr_ch, char *);
+
+		if (str == NULL)
+			prinf("(nil)");
+		else
+			printf("%s", str);
+
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+
+	va_end(array_ch);
 }
