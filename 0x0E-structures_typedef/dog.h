149ea09ec38bc81d767f60a454af11e2fc667b91
abraham23o <abraonyonka23@gmail.com>
2023-10-13 15:52:52 +0300

    new task - structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - info about dog
+ * @name: 1st element
+ * @age: 2nd element
+ * @owner: 3rd element
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
