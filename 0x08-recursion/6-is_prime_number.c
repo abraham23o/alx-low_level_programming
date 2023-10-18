20f5df4226f516c57d5f2709cad263ee31bab4d5
abraham23o <abraonyonka23@gmail.com>
2023-10-18 11:44:54 +0300

    find prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
 
+int prime_checker(int, int);
+
 /**
- * is_prime_number - check for prime numbers
- * @n: number to check
- * Return: 1 if n is prime number, 0 false
+ * is_prime_number - check if a number is a prime number
+ * @n: number to be checked
+ * Return: 1 if true, 0 if false
  */
 int is_prime_number(int n)
 {
-	int i;
-
-	if (i == 1)
-		return (1);
-
-	if (n % i == 0 && i > 0)
+	if (n <= 1)
 		return (0);
 
-	i -= 1;
+	return (prime_checker(n, n - 1));
+}
 
-	return (is_prime_number(n));
+/**
+ * prime_checker - check if a number is a prime number
+ * @num: number to be checked
+ * @div: divisor of num
+ * Return: 1 if true, 0 if false
+ */
+int prime_checker(int num, int div)
+{
+	if (div <= 1)
+		return (1);
+
+	if (num % div == 0)
+		return (0);
+
+	return (prime_checker(num, div - 1));
 }
