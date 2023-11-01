101779b50dc9bfc7f56cbb739710402d0cd6039f
abraham23o <abraonyonka23@gmail.com>
2023-11-01 07:27:14 +0300

    refactor read textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,40 +13,40 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int result;
-	char *tmp;
-	ssize_t r, w;
+	int fd;
+	char *buffer;
+	ssize_t fdr, fdw;
 
 	if (filename == NULL)
 		return (0);
 
-	tmp = malloc(sizeof(char) * letters);
-	if (tmp == NULL)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 		return (0);
 
-	result = open(filename, O_RDONLY);
-	if (result < 0)
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
 	{
-		free(tmp);
+		free(buffer);
 		return (0);
 	}
 
-	r = read(result, tmp, letters);
-	if (r < 0)
+	fdr = read(fd, buffer, letters);
+	if (fdr < 0)
 	{
-		free(tmp);
+		free(buffer);
 		return (0);
 	}
 
-	w = write(STDOUT_FILENO, tmp, r);
-	if (w < 0)
+	fdw = write(STDOUT_FILENO, buffer, fdr);
+	if (fdw < 0)
 	{
-		free(tmp);
+		free(buffer);
 		return (0);
 	}
 
-	close(result);
-	free(tmp);
+	close(fd);
+	free(buffer);
 
-	return (w);
+	return (fdw);
 }
