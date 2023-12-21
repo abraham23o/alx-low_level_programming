12011acda25dd5e5ee9f7cbb45947af99ae168d5
abraham23o <abraonyonka23@gmail.com>
2023-12-21 18:24:03 +0300

    refactor create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,14 +7,24 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i = 0;
+	hash_table_t *hash_table = NULL;
 
-	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
+	if (size < 1)
+		return (NULL);
+
+	hash_table = malloc(sizeof(hash_table_t));
 
 	if (hash_table == NULL)
 		return (NULL);
 
 	hash_table->size = size;
 	hash_table->array = malloc(sizeof(hash_node_t) * size);
+
+	if (hash_table->array == NULL)
+	{
+		free(hash_table);
+		return (NULL);
+	}
 
 	for (; i < size; ++i)
 		hash_table->array[i] = NULL;
