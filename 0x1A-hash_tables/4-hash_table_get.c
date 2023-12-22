95ea7a7b836a2ed27dfb33c0313348c84a08a0fc
abraham23o <abraonyonka23@gmail.com>
2023-12-22 13:44:02 +0300

    get value of key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieve value in a hash table using a key
+ * @ht: hash table to insert key:value
+ * @key: key to insert to the hash table
+ * Return: value of a key
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int slot = 0;
+	hash_node_t *entry = NULL;
+
+	slot = key_index((const unsigned char *)key, ht->size);
+	entry = ht->array[slot];
+
+	if (entry == NULL)
+		return (NULL);
+
+	while (entry != NULL)
+	{
+		if (strcmp(entry->key, key) == 0)
+			return (entry->value);
+
+		entry = entry->next;
+	}
+	return (NULL);
+}
